04d5dfc33fc5e2d698158839ffdf9a328a3aa518
Balaji250692 <yuvaraghavan87@gmail.com>
2020-12-19 22:04:50 +0530

    comitted on sat

---
+++ src/org/znassy/test/login/CompanyInfo.java
@@ -0,0 +1,20 @@
+package org.znassy.test.login;
+
+public class CompanyInfo {
+ private void companyName() {
+	 System.out.println("events");
+}
+ private void companyId() {
+ System.out.println("acc12");
+}
+  private void companyAdd() {
+	  System.out.println("bcc12");
+}
+ //executing 
+  public static void main(String[] args) {
+	CompanyInfo id= new CompanyInfo();
+	id.companyName();
+	id.companyId();
+	id.companyAdd();
+}
+}
